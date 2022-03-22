fd66ff372fa0921b60739d903979eb4b30ddc0dd
Abby <ba40431@gmail.com>
2022-03-22 15:12:43 +0800

    Part-4 by using JWT

--- routes/token.py
+++ routes/token.py
@@ -1,12 +1,22 @@
+from flask import *
 import jwt
 import time
 
-def make_token(email):
-    key="secretkey"
-    now=time.time()
-    expiretime=60*60
-    payload={
-    "email":email,
-    "expire":now+expiretime
-    }
-    return jwt.encode(payload,key,algorithm = 'HS256')
+class UseJWT:
+    def make_token(self,id,name,email):
+        key="secretkey"
+        now=time.time()
+        expiretime=60*60
+        payload={
+            "id":id,
+            "name":name,
+            "email":email,
+            "expire":now+expiretime
+        }
+        token=jwt.encode(payload,key,algorithm="HS256")
+        return token
+    def get_cookie(self):
+        jwt=request.cookies.get('token')
+        return jwt
+
+use_jwt=UseJWT()
